ffc662143f4306f25d08082b15e19ab3c6599f0c
Maya Gans <maya.gans@atorusresearch.com>
2021-03-05 21:28:18 +0000

    adding broken test

--- tests/testthat/test-validators.R
+++ tests/testthat/test-validators.R
@@ -1,3 +1,37 @@
-test_that("check columns works", {
-   check_columns
+# where should this function go
+empty_df <- function(nms, fill) {
+   df <- as.data.frame(matrix(fill,1,length(nms)))
+   names(df) <- nms
+   return(df)
+}
+
+#... why doesnt this work?
+test_that("empty columns fail primitive checks", {
+
+   dfs <- purrr::map(col_vars("ds_spec",
+                              "ds_vars",
+                              "var_spec",
+                              "value_spec",
+                              "derivations",
+                              "codelist",
+                              "changelog"),
+                     ~ empty_df(.x, fill = ""))
+
+   expect_warning(do.call(check_columns, dfs[-7]))
 })
+
+
+test_that("NA columns fail", {
+
+   dfs <- purrr::map(col_vars("ds_spec",
+                              "ds_vars",
+                              "var_spec",
+                              "value_spec",
+                              "derivations",
+                              "codelist",
+                              "changelog"),
+                     ~ empty_df(.x, fill = NA))
+
+   expect_error(suppressWarnings(do.call(check_columns, dfs[-7])))
+})
+
