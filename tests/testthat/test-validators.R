15e6b0f6ff52fe463e6b236e2706ebf4bdbd2400
Maya Gans <maya.gans@atorusresearch.com>
2021-03-05 21:34:48 +0000

    making tests that dont work

--- tests/testthat/test-validators.R
+++ tests/testthat/test-validators.R
@@ -5,17 +5,20 @@ empty_df <- function(nms, fill) {
    return(df)
 }
 
-#... why doesnt this work?
+# both of these functions only work
+# when the data def object is loaded
+# and i think its checking the wrong thing
+
 test_that("empty columns fail primitive checks", {
 
-   dfs <- purrr::map(col_vars("ds_spec",
-                              "ds_vars",
-                              "var_spec",
-                              "value_spec",
-                              "derivations",
-                              "codelist",
-                              "changelog"),
-                     ~ empty_df(.x, fill = ""))
+   dfs <- purrr::map(col_vars(), ~ empty_df(.x, fill = "")) %>%
+      setNames(c("ds_spec",
+               "ds_vars",
+               "var_spec",
+               "value_spec",
+               "derivations",
+               "codelist",
+               "changelog"))
 
    expect_warning(do.call(check_columns, dfs[-7]))
 })
@@ -23,14 +26,14 @@ test_that("empty columns fail primitive checks", {
 
 test_that("NA columns fail", {
 
-   dfs <- purrr::map(col_vars("ds_spec",
-                              "ds_vars",
-                              "var_spec",
-                              "value_spec",
-                              "derivations",
-                              "codelist",
-                              "changelog"),
-                     ~ empty_df(.x, fill = NA))
+   dfs <- purrr::map(col_vars(), ~ empty_df(.x, fill = NA)) %>%
+      setNames(c("ds_spec",
+                 "ds_vars",
+                 "var_spec",
+                 "value_spec",
+                 "derivations",
+                 "codelist",
+                 "changelog"))
 
    expect_error(suppressWarnings(do.call(check_columns, dfs[-7])))
 })
