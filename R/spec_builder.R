3333f9b2f19c046408d72ddee66223a81cd408de
Christina Fillmore <christina.e.fillmore@gsk.com>
2021-03-17 11:24:37 +0000

    Working on updates to the spec builders, finished ds, ds_vars, and var_spec needs additional tables

--- R/spec_builder.R
+++ R/spec_builder.R
@@ -1,80 +1,119 @@
 
 
+#' Check the type of spec document
+#'
+#' @param path file location as a string
+#'
+#' @return returns string indicating the type of spec document
+#'
+#' @noRd
+spec_type <- function(path){
+   sheets <- excel_sheets(path)
+   if(!any(sheets %>% str_detect("[D|d]omains?|[D|d]atasets?"))){
+      stop("File does not contain a Domain/Datasets tab, which is needed. Please either modify the spec document or write a reader (see documentation for more information)",
+           call. = FALSE)
+   } else if(any(sheets %>% str_detect("ADSL|DM"))){
+      type <- "by_ds"
+   } else if(any(sheets %>% str_detect("[V|v]ariables?"))){
+      type <- "by_type"
+   } else {
+      stop("File in an unknown format. Please either modify the spec document or write a reader (see documentation for more information)",
+           call. = FALSE)
+   }
+   type
+}
+
+
+
+
 #' Read information from spec document
 #'
 #' @param path string of the file path
 #'
 #' @return
-#' @export
-read_spec1 <- function(path){
+#' @noRd
+read_all_sheets <- function(path){
    sheets <- excel_sheets(path)
-   dom <- sheets %>%
-      sheet_name("[D|d]omains?")
-   spec <- sheets %>%
-      sheet_name("[S|s]pec[ification|s]?")
-   var <- sheets %>%
-      sheet_name("[V|v]ar.*")
-   val_meta <- sheets %>%
-      sheet_name("[V|v]al[ue]?.*")
-   ct <- sheets %>%
-      sheet_name("CT|ct|[C|c]ontrolled.*")
-   derviation <- sheets %>%
-      sheet_name(".*Method|[D|d]erivations?")
-   to_read <- c(dom, spec, var, val_meta, ct, derviation)
-
-   data <- to_read %>% map(~read_excel(path, sheet = .))
-   names(data) <- c("dom", "spec", "var", "val_meta", "ct", "derviation")
-   data
+   all_dat <- sheets %>%
+      map(~read_excel(path, sheet = .))
+   names(all_dat) <- sheets
+   all_dat
 }
 
 
-#      .var_spec = c("variable", "length", "label", "type", "common"),
-#      .value_spec = c("type", "origin", "code_id", "dataset", "variable", "where", "derivation_id"),
-#      .derivations = c("derivation_id", "derivation"),
-#      .codelist= c("code_id", "names","type", "codes"),
-#      .change_log = c("table_chg", "column_chg", "what_chg"))
-
-spec1_to_ds_spec <- function(doc){
-   doc$dom %>%
-      rename(dataset = matches("[N|n]ame|dataset|[D|d]omain"),
-             structure = matches("[S|s]tructure"),
-             label = matches("[L|l]abel")) %>%
-      select(dataset, structure, label) %>%
+spec_type_to_ds_spec <- function(doc){
+   cols <- c("dataset" = "[N|n]ame|[D|d]ataset|[D|d]omain",
+             "structure" = "[S|s]tructure",
+             "label" = "[L|l]abel|[D|d]escription")
+   create_tbl(doc, cols) %>%
       distinct()
 }
 
-spec1_to_ds_vars <- function(doc){
-   doc$spec %>%
-      rename(dataset = matches("dataset|[D|d]omain"),
-             variable = matches("[V|v]ariable [N|n]ame"),
-             key_seq = matches("[V|v]ariable [O|o]rder|[S|s]eq"),
-             keep = matches("[K|k]eep|[M|m]andatory"),
-             core = matches("[C|c]ore|[R|r]equired")) %>%
-      select(dataset, variable, key_seq, keep, core) %>%
+spec_type_to_ds_vars <- function(doc, cols = NULL){
+   if(is.null(cols)){
+      cols <- c("dataset" = "[D|d]ataset|[D|d]omain",
+                "variable" = "[V|v]ariable [[N|n]ame]?|[V|v]ariables?",
+                "key_seq" = "[V|v]ariable [O|o]rder|[S|s]eq|[O|o]rder",
+                "keep" = "[K|k]eep|[M|m]andatory")
+   }
+   #TODO add error or warning if not named?
+
+   var_sheet <- str_subset(names(doc), "[V|v]ar")
+   doc[var_sheet] %>%
+      create_tbl(cols) %>%
       distinct()
 }
 
-spec1_to_var_spec <- function(doc){
-   var_df <- doc$var %>%
-      rename(variable = matches("[N|n]ame"),
-             length = matches("[L|l]ength"),
-             label = matches("[L|l]abel"),
-             type = matches("[T|t]ype") ) %>%
-      select(variable, length, label, type) %>%
-      distinct()
-   common_vars <- doc$dom$`Key Variables` %>%
-      str_split(",") %>%
-      map(str_trim)
-   common_vars <- common_vars[[1]]%>%
-      keep(function(x){
-         common_vars%>% map_lgl(~x%in% .) %>% all()
-      })
+spec_type_to_var_spec <- function(doc, col = NULL){
+   if(is.null(cols)){
+      cols <- c("variable" = "[N|n]ame|[V|v]ariables?",
+                "length" = "[L|l]ength",
+                "label" = "[L|l]abel",
+                "type" = "[T|t]ype",
+                "dataset" = "[D|d]ataset|[D|d]omain")
+   }
+   out <- create_tbl(doc, cols)
+   if(!"dataset" %in% names(out)){
+      dups <- out %>%
+         distinct() %>%
+         group_by(variable) %>%
+         summarise(n = n(), .groups = "drop") %>%
+         filter(n > 1)
+      if(nrow(dups) > 0){
+         dups %>%
+            pull(variable) %>%
+            paste(collapse = "\n") %>%
+            paste0("The following variables are repeated with different metadata for different datasets:\n",
+                   ., "\nPlease add 'dataset' = [Name of dataset column] to your named cols vector, to correct for this") %>%
+            stop(., call. = FALSE)
+      }
+   } else {
+      if(!"common" %in% names(cols)){
+         # Get the variable common to all datasets can only be calculated with ds present
+         common_vars <- out %>%
+            group_by(dataset) %>%
+            select(dataset, variable) %>%
+            group_split(.keep = FALSE) %>%
+            reduce(inner_join, by = "variable") %>%
+            mutate(common = TRUE)
+         out <- out %>%
+            left_join(common_vars, by = "variable") %>%
+            replace_na(list(common = FALSE))
+      }
 
-   var_df %>%
-      mutate(common = variable %in% common_vars)
+
+      out <- out %>%
+         distinct(variable, length, label, type, .keep_all = TRUE) %>%
+         group_by(variable) %>%
+         mutate(n = n(),
+                variable = if_else(n > 1, paste0(dataset, ".", variable),
+                                   variable)) %>%
+         select(-n, -dataset)
+   }
+   out
 }
 
-spec1_to_value_spec <- function(doc){
+spec_type_to_value_spec <- function(doc){
    doc$val_meta %>%
       rename(ds_var = matches("VLM [N|n]ame"),
              origin = matches("[O|o]rigin"),
@@ -93,7 +132,7 @@ spec1_to_value_spec <- function(doc){
       select(-dev)
 }
 
-spec1_to_code_list <- function(doc){
+spec_type_to_code_list <- function(doc){
    doc$ct %>%
       rename(code_id = "Codelist Code",
              names = matches("[N|n]ame"),
@@ -109,7 +148,7 @@ spec1_to_code_list <- function(doc){
                              codes))
 }
 
-spec1_to_derivations <- function(doc){
+spec_type_to_derivations <- function(doc){
    doc$val_meta %>%
       rename(ds_var = matches("VLM [N|n]ame"),
              origin = matches("[O|o]rigin"),
@@ -128,19 +167,60 @@ spec1_to_derivations <- function(doc){
 }
 
 
-#' Get name of sheet
+#' Select sheet
 #'
-#' @param sheets list of all sheet names
-#' @param regex regex to parse out desired sheet
+#' @param doc list of sheets from a excel dos
+#' @param cols vector of regex to get a datasets base on which columns it has.
+#'   If the vector is named it will also rename the columns
 #'
-#' @return string of the matching
+#' @return dataset (or list of datasets if not spefic engough)
 #' @noRd
-sheet_name <- function(sheets, regex){
-   sheet_name <- sheets %>%
-      str_extract(regex) %>%
-      discard(is.na)
-   if(length(sheet_name) > 1){
-      stop("Sheet names don't follow required naming convention. Please look at the documentation and change accordingly")
+create_tbl <- function(doc, cols){
+   matches <- doc %>%
+      keep(function(x){
+         cols %>%
+            map_lgl(~any(str_detect(names(x), .))) %>%
+            all()
+      })
+   if(length(matches) == 0) {
+      # Get which variable can't be matches
+      mismatch_per_sheet <- doc %>%
+         map(function(x){
+            cols %>%
+               map_lgl(~any(str_detect(names(x), .))) %>%
+               discard(~.) # Remove the matched values
+         })
+      # Find the closest sheet by looking for the sheet(s) with the fewest mismatches
+      mis_lens <- mismatch_per_sheet %>%
+         map_int(length)
+      closest_sheets <- mis_lens %>%
+         keep(~ . == min(mis_lens)) %>%
+         names()
+      # Get the name of the sheets and which columns don't match
+      sheets_to_error <- mismatch_per_sheet %>%
+         keep(names(.) %in% closest_sheets)
+
+      # Write out the error
+      sheets_to_error %>%
+         map2_chr(names(sheets_to_error), function(vars, sheet_name){
+            paste0("Sheet '", sheet_name, "' is the closest match, but unable to match the following column(s)",
+                   paste(names(vars), collapse = ","))
+         }) %>%
+         paste0(collapse = "\n") %>%
+         paste0("Unable to identify a sheet with all cloumns.\n", . ) %>%
+         stop(call. = FALSE)
+
+   } else if(length(matches) == 1){
+      matches[[1]] %>%
+         select(matches(cols))
+   } else {
+      sheets_mats <- matches %>%
+         names()
+      paste("Column names are not specific enough to identify a single sheet. The following",
+            length(sheets_mats),
+            "match the criteria set:", paste(sheets_mats, collapse = ", ")) %>%
+         warning(., call. = FALSE)
+      matches %>%
+         map(~select(., matches(cols)))
    }
-   sheet_name
 }
