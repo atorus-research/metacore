42056d3bbac1525f5a17fb0ed270356d566220b4
Christina Fillmore <christina.e.fillmore@gsk.com>
2021-04-28 14:45:47 +0100

    Change imports

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,14 @@
 #' @import tidyr
 #' @import dplyr
 #' @import stringr
+#' @import rlang
 #' @importFrom XML xmlNamespaceDefinitions xmlGetAttr xmlElementsByTagName xmlValue xmlTreeParse getNodeSet xmlToDataFrame
 #' @import purrr
 #' @importFrom stats var
-#' @importFrom rlang as_string prim_name
+# @importFrom rlang as_string prim_name
 #' @importFrom readxl excel_sheets read_excel
 #' @importFrom tibble tibble as_tibble
+#' @importFrom readxl excel_sheets read_excel
 
 NULL
 
