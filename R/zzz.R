367e5c4309797fce7697e7d2cea30a9e80334f9f
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2021-07-28 01:41:34 +0000

    Updates related to elimination of c_across

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 #' @import R6
 #' @importFrom tidyr replace_na nest unnest everything
-#' @importFrom dplyr right_join n_distinct row_number case_when n if_else arrange rowwise c_across anti_join inner_join bind_rows distinct mutate full_join left_join group_by summarise filter pull select group_split ungroup
+#' @importFrom dplyr right_join n_distinct row_number case_when n if_else arrange rowwise anti_join inner_join bind_rows distinct mutate full_join left_join group_by summarise filter pull select group_split ungroup
 #' @importFrom tibble tibble tribble
-#' @importFrom stringr str_replace str_count str_remove_all str_remove str_detect str_subset str_length str_replace_na str_replace_all str_extract str_c str_trim str_split
+#' @importFrom stringr str_replace str_count str_remove_all str_remove str_detect str_subset str_length str_replace_na str_replace_all str_extract str_c str_trim str_split str_starts
 #' @importFrom rlang !! as_string expr sym prim_name
 #' @importFrom XML xmlNamespaceDefinitions xmlGetAttr xmlElementsByTagName xmlValue xmlTreeParse getNodeSet xmlToDataFrame
-#' @importFrom purrr keep discard reduce compact map map_chr map_lgl map_dfr map_int map2_lgl map2_chr
+#' @importFrom purrr keep discard reduce compact map map_chr map_lgl map_dfr map_int map2_lgl map2_chr pmap_chr
 #' @importFrom stats var na.omit
 #' @importFrom readxl excel_sheets read_excel
 #' @importFrom tibble tibble as_tibble
