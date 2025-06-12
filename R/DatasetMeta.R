265d353692bc37f4d3a8cb1d5e1d61a397c81165
Liam Hobby <liam.f.hobby+gsk@gsk.com>
2025-06-12 16:04:45 +0100

    Improve formatting of print. Add `quiet` to greet fcn.

--- R/DatasetMeta.R
+++ R/DatasetMeta.R
@@ -9,17 +9,18 @@
 DatasetMeta <- R6::R6Class("DatasetMeta",
   inherit = MetaCore,
   private = list(
-     name  = NA,
-     label = NA,
-     num_vars = NA,
+     .name  = NA,
+     .label = NA,
+     .num_vars = NA,
 
-     greet = function() {
-        cli_alert_success("{private$name} dataset successfully selected")
+     .greet = function(quiet) {
+        cli_alert_success("{private$.name} dataset successfully selected")
+        if (quiet) cli_alert_warning(col_red("Dataset metadata specification subsetted with suppressed warnings"))
      }
   ),
 
   public = list(
-     initialize = function(metacore) {
+     initialize = function(metacore, quiet = FALSE) {
         super$initialize(
            ds_spec = metacore$ds_spec,
            ds_vars = metacore$ds_vars,
@@ -29,28 +30,32 @@ DatasetMeta <- R6::R6Class("DatasetMeta",
            codelist = metacore$codelist,
            supp = metacore$supp
         )
-        private$name  = metacore$ds_spec$dataset[1]
-        private$label = metacore$ds_spec$label[1]
-        private$num_vars = metacore$ds_vars |> nrow()
+        private$.name  = metacore$ds_spec$dataset[1]
+        private$.label = metacore$ds_spec$label[1]
+        private$.num_vars = metacore$ds_vars |> nrow()
 
-        private$greet()
+        private$.greet(quiet)
      },
 
      print = function() {
+        tables <- ls(envir = self)
+
         cli_par()
-        cli_rule(left = "Dataset specification object for: {private$name} ({private$label})")
-        cli_text("The dataset contains {private$num_vars} variables")
+        cli_rule(left = "Dataset specification object for {private$.name} ({private$.label})")
+        cli_text("The dataset contains {private$.num_vars} variables")
         cli_end()
 
         cli_par()
-        cli_rule(left = "The structure of the dataset specification object is")
-        tables <- ls(envir = self)
+        cli_text("The structure of the specification object is:")
         for (table in tables) {
            obj <- get(table, envir = self)
            if (!is.list(obj)) { next }
-
-           cli_text("{table}: {typeof(table)} [{dim(obj)[1]} x {dim(obj)[2]}] {paste(names(obj), collapse = ' ')}")
+           cli_bullets(c(">" = "{table}: {typeof(table)} [{dim(obj)[1]} x {dim(obj)[2]}] {paste(names(obj), collapse = ' ')}"))
         }
+        cli_end()
+
+        cli_div(theme = list(span.emph = list(color = "orange")))
+        cli_text("To inspect the specification object use {.emph View(<name>)} in the console.")
         cli_end()
      }
   )
