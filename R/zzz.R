874645bd4b79cc2bb1d413ea41074b693ef43ba6
Jeff Dickinson <jeff.dickinson@navitaslifesciences.com>
2025-12-19 13:37:40 -0600

    #136 first style files and run documentation

--- R/zzz.R
+++ R/zzz.R
@@ -14,32 +14,34 @@
 #' @importFrom xml2 read_xml xml_find_all xml_find_first xml_attr xml_ns_strip xml_text
 NULL
 
-globalVariables(c("private",
-                  "self",
-                  "keep",
-                  ".",
-                  ".data",
-                  "code",
-                  "code_id",
-                  "codes",
-                  "core",
-                  "dataset",
-                  "decode",
-                  "derivation_id",
-                  "dictionary",
-                  "id",
-                  "key_seq",
-                  "lab",
-                  "label",
-                  "matches",
-                  "spec_type_to_code_list",
-                  "type",
-                  "variable",
-                  "where",
-                  "where_new",
-                  "var1",
-                  "n_lab"))
+globalVariables(c(
+  "private",
+  "self",
+  "keep",
+  ".",
+  ".data",
+  "code",
+  "code_id",
+  "codes",
+  "core",
+  "dataset",
+  "decode",
+  "derivation_id",
+  "dictionary",
+  "id",
+  "key_seq",
+  "lab",
+  "label",
+  "matches",
+  "spec_type_to_code_list",
+  "type",
+  "variable",
+  "where",
+  "where_new",
+  "var1",
+  "n_lab"
+))
 
 .onAttach <- function(libname, pkgname) {
-   packageStartupMessage("Attaching package `metacore`\n\nAs of metacore 0.3.0 the `keep` variable in the `ds_vars` table has been renamed to `mandatory`. Please see release documentation for details.")
+  packageStartupMessage("Attaching package `metacore`\n\nAs of metacore 0.3.0 the `keep` variable in the `ds_vars` table has been renamed to `mandatory`. Please see release documentation for details.")
 }
